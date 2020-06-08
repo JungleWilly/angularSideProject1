0aee26891d1dc6c415c1f8309d83df29e806fbcc
junglewilly <guillaumec.dev@gmail.com>
2020-06-08 22:45:25 +0200

    add dancer-details component linked to its parent component

--- src/app/dancer-details/dancer-details.component.ts
+++ src/app/dancer-details/dancer-details.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Danseur } from '../dancers/dancer';
 
 @Component({
   selector: 'app-dancer-details',
   templateUrl: './dancer-details.component.html',
-  styleUrls: ['./dancer-details.component.css']
+  styleUrls: ['./dancer-details.component.css'],
 })
 export class DancerDetailsComponent implements OnInit {
+  @Input() dancer: Danseur;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
